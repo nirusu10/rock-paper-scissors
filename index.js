af11c33d4f19548408e95dbb7e23b57621473912
Nils Matic <matic1909@gmail.com>
2024-11-05 14:41:25 +0100

    wrote getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello from script!')
+const CHOICES = ['rock', 'paper', 'scissors']
+
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * 3)
+  return CHOICES[randomIndex]
+}
