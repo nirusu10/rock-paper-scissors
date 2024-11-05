75989738ca176536fcb580d2122f99a1edb7c743
Nils Matic <matic1909@gmail.com>
2024-11-05 15:14:08 +0100

    added playGame() function to play a given number of games

--- index.js
+++ index.js
@@ -20,45 +20,58 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
   // result is 0 for a draw, 1 for a human win and -1 for a computer win
-  console.log(`Player chose ${humanChoice}. Computer chose ${computerChoice}`)
-  let result = 0
 
   switch (humanChoice) {
     case 'rock':
       if (computerChoice === 'scissors') {
-        result = 1
-        break
+        return 1
       } else if (computerChoice === 'paper') {
-        result = -1
-        break
+        return -1
       }
     case 'paper':
       if (computerChoice === 'rock') {
-        result = 1
-        break
+        return 1
       } else if (computerChoice === 'scissors') {
-        result = -1
-        break
+        return -1
       }
     case 'scissors':
       if (computerChoice === 'paper') {
         result = 1
         break
       } else if (computerChoice === 'rock') {
-        result = -1
-        break
+        return -1
       }
   }
 
-  if (result == 0) {
-    console.log(`It's a draw. Both players chose ${humanChoice}`)
-  } else if (result == -1) {
-    computerScore++
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-  } else {
-    humanScore++
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-  }
+  return 0
+}
 
-  console.log(`CURRENT SCORE: Human ${humanScore} - ${computerScore} Computer`)
+const playGame = (rounds) => {
+  let round = 1
+  while (round <= rounds) {
+    console.log('ROUND ' + round + ':')
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+    console.log('Player chose ' + humanChoice)
+    console.log('Computer chose ' + computerChoice)
+    const result = playRound(humanChoice, computerChoice)
+
+    if (result == 0) {
+      console.log(`It's a draw. Both players chose ${humanChoice}`)
+    } else if (result == -1) {
+      computerScore++
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    } else {
+      humanScore++
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    }
+
+    if (round != rounds)
+      console.log(
+        `CURRENT SCORE: Human ${humanScore} - ${computerScore} Computer`
+      )
+
+    round++
+  }
+  console.log(`FINAL SCORE: HUMAN ${humanScore} - ${computerScore} COMPUTER`)
 }
