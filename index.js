1a462a2f83f0e3cee14cbc17b002897cb5d81773
Nils Matic <matic1909@gmail.com>
2024-11-05 14:44:33 +0100

    added getHumanChoice function

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3)
   return CHOICES[randomIndex]
 }
+
+function getHumanChoice() {
+  let choice = ''
+  while (!CHOICES.includes(choice)) {
+    choice = window
+      .prompt('What will your choice be? Rock, paper or scissors?')
+      .toLowerCase()
+  }
+  return choice
+}
